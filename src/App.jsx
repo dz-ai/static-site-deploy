726185571e4b89758409cbda452ee30e30f9e5ab
David Zim <davidzim29.90@gmail.com>
2022-12-11 21:13:31 +0200

    some editions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {NavLink} from "react-router-dom";
 
 function App() {
 
@@ -6,6 +7,10 @@ function App() {
     <div className="App">
         <h1>my static site</h1>
         <img src="https://media.istockphoto.com/id/1154370446/photo/funny-raccoon-in-green-sunglasses-showing-a-rock-gesture-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=kkZiaB9Q-GbY5gjf6WWURzEpLzNrpjZp_tn09GB21bI=" alt="funny monky"/>
+        <br/>
+        <NavLink to="/second">
+            <button>second page</button>
+        </NavLink>
     </div>
   )
 }
