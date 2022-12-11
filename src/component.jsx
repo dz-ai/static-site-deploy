387c7adb3fdca31f78f69dd83bfeddcc102a266f
David Zim <davidzim29.90@gmail.com>
2022-12-11 19:07:32 +0200

    add another page

---
+++ src/component.jsx
@@ -0,0 +1,8 @@
+
+export const Component = () => {
+    return (
+        <div>
+            <h2>second page</h2>
+        </div>
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+    BrowserRouter,
+    createBrowserRouter, Route, Router,
+    RouterProvider, Routes} from "react-router-dom";
+import {Component} from "./component.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<App />}/>
+              <Route path="/second" element={<Component />}/>
+            </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
 )
