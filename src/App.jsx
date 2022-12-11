d9fde2b7af48957148406a1c5d4b62201037d9cb
David Zim <davidzim29.90@gmail.com>
2022-12-11 18:18:20 +0200

    added img

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
         <h1>my static site</h1>
+        <img src="https://media.istockphoto.com/id/1154370446/photo/funny-raccoon-in-green-sunglasses-showing-a-rock-gesture-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=kkZiaB9Q-GbY5gjf6WWURzEpLzNrpjZp_tn09GB21bI=" alt="funny monky"/>
     </div>
   )
 }
